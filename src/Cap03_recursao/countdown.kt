411b50ab45a5cc3ec30241111011fe8b46d30d80
Aline Ferreira <alinfst@gmail.com>
2024-05-09 22:41:08 -0300

    add countdown exercise

---
+++ src/Cap03_recursao/countdown.kt
@@ -0,0 +1,14 @@
+package Cap03_recursao
+
+private fun countdown(i: Int){
+    println(i)
+    when{
+        //base case
+        i <= 0 -> return
+        //recursive case
+        else -> countdown(i-1)
+    }
+}
+fun main() {
+    countdown(5)
+}
